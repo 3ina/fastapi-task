306526aa5c7c3139993980c5947257a3f174c853
sinaroydel <3inaroydl@gmail.com>
2025-10-01 06:09:57 +0330

    add settings for fastapi app

---
+++ app/core/config.py
@@ -0,0 +1,18 @@
+import os
+from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+
+env_file = ".env"
+load_dotenv(env_file)
+
+
+class Settings(BaseSettings):
+    APP_PORT: int = os.getenv("PORT")
+    ENVIRONMENT: str = os.getenv("ENVIRONMENT")
+    DEBUG: bool = os.getenv("DEBUG")
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    LOG_LEVEL: str = os.getenv("LOG_LEVEL")
+
+
+settings = Settings()
