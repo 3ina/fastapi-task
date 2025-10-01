94abe2b635a401687ab5d18a4cdebcf50b9750b3
sinaroydel <3inaroydl@gmail.com>
2025-10-01 08:59:25 +0330

    initial Fastapi app

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from fastapi-task!")
+from fastapi import FastAPI
+from app.core.config import settings
+from contextlib import asynccontextmanager
+from app.core.database import Base, engine
 
 
-if __name__ == "__main__":
-    main()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+
+app = FastAPI(
+    title="Top company task API",
+    docs_url="/docs",
+    redoc_url="/redoc",
+    debug=settings.DEBUG,
+    lifespan=lifespan,
+)
