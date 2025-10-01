98426d019cae24ebd1edaee0063cc2fcfe533aae
sinaroydel <3inaroydl@gmail.com>
2025-10-01 06:10:25 +0330

    create engine and connection for db

---
+++ app/core/database.py
@@ -0,0 +1,27 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.orm import declarative_base
+from app.core.config import settings
+
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    echo=False,
+    future=True,
+    pool_pre_ping=True,
+    pool_recycle=300,
+    pool_size=5,
+    max_overflow=10,
+)
+
+async_session_local = async_sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
+
+Base = declarative_base()
+
+
+async def get_db():
+    async with async_session_local() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
