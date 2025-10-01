cd10ff4bdc758296063d97f3b288a5596891d9f6
sinaroydel <3inaroydl@gmail.com>
2025-10-01 14:28:50 +0330

    add order repo

---
+++ app/infrastructure/orderRepository.py
@@ -0,0 +1,21 @@
+from typing import Optional
+from pydantic import BaseModel
+from app.infrastructure.baseRepository import BaseRepository
+from app.infrastructure.models import OrderORM
+
+
+class CreateOrderSchema(BaseModel):
+    code: str
+    price: float
+    user_id: int
+
+
+class UpdateOrderSchema(BaseModel):
+    code: Optional[str] = None
+    price: Optional[float] = None
+    user_id: Optional[int] = None
+
+
+class OrderRepository(BaseRepository[OrderORM, CreateOrderSchema, UpdateOrderSchema]):
+    def update(self, *args, **kwargs):
+        raise NotImplementedError("orders are immutable dosen't updated")
