5f32e2309c7daacccbddd7de311578b97f49c3e4
sinaroydel <3inaroydl@gmail.com>
2025-10-02 16:22:56 +0330

    add excption for user service

---
+++ app/exceptions/userService.py
@@ -0,0 +1,20 @@
+# app/services/exceptions.py
+
+
+class ServiceException(Exception):
+    pass
+
+
+class UserAlreadyExistsException(ServiceException):
+    def __init__(self, field: str, value: str):
+        self.field = field
+        self.value = value
+        super().__init__(f"User with {field} '{value}' already exists.")
+
+
+class IncorrectCredentialsException(ServiceException):
+    pass
+
+
+class IncorrectPasswordException(ServiceException):
+    pass
