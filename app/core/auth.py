6e0c7f47bdff33e17b5e444b5806a69c8c5fc15f
sinaroydel <3inaroydl@gmail.com>
2025-10-02 11:10:31 +0330

    add function for create refresh token

--- app/core/auth.py
+++ app/core/auth.py
@@ -1,8 +1,9 @@
 from app.core.config import settings
 from passlib.context import CryptContext
 from datetime import datetime, timedelta, timezone
+from jose import jwt, JWTError
 
-SECERT_KEY = settings.SECRET_KEY
+SECRET_KEY = settings.SECRET_KEY
 ALGORITHM = "HS256"
 
 ACCESS_TOKEN_EXPIRES_SECONDS = 1200
@@ -32,3 +33,22 @@ def create_access_token(data: dict, expires_delta: int = ACCESS_TOKEN_EXPIRES_SE
             "type": "access",
         }
     )
+
+    return jwt.encode(to_encode, SECRET_KEY, ALGORITHM)
+
+
+def create_refresh_token(
+    data: dict, expires_delta: int = REFRESH_TOKEN_EXPIRES_SECONDS
+) -> str:
+    """
+    set expires_delta in second
+    """
+
+    to_encode = data.copy()
+    to_encode.update(
+        {
+            "exp": datetime.now(timezone.utc) + timedelta(seconds=expires_delta),
+            "type": "refresh",
+        }
+    )
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
