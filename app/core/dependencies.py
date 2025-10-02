eafb507c36c2785c12b6ac88185c879f4d37d1ca
sinaroydel <3inaroydl@gmail.com>
2025-10-02 16:24:14 +0330

    create dependency for UserService and UserRepository

---
+++ app/core/dependencies.py
@@ -0,0 +1,17 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.core.database import get_db
+from app.services.userService import UserService
+from app.infrastructure.userRepository import UserRepository
+from app.infrastructure.models import UserORM
+from fastapi import Depends
+
+
+def get_user_repo(db: AsyncSession = Depends(get_db)):
+    repo = UserRepository(model=UserORM, db=db)
+    return repo
+
+
+def get_user_service(user_repo: UserRepository = Depends(get_user_repo)) -> UserService:
+    service = UserService(user_repo=user_repo)
+    return service
+
