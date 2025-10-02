b685add465bca325f17806c9ddc6dc20c204c01d
sinaroydel <3inaroydl@gmail.com>
2025-10-02 18:16:33 +0330

    add method for get user with username

--- app/infrastructure/userRepository.py
+++ app/infrastructure/userRepository.py
@@ -1,5 +1,6 @@
 from typing import Optional
 from pydantic import BaseModel
+from sqlalchemy import select
 from app.infrastructure.baseRepository import BaseRepository
 from app.infrastructure.models import UserORM
 
@@ -12,11 +13,14 @@ class CreateUserSchema(BaseModel):
 
 
 class UpdateUserSchema(BaseModel):
-    username: Optional[str] = None
-    password: Optional[str] = None
-    name: Optional[str] = None
-    phone_number: Optional[str] = None
+    username: str | None = None
+    password: str | None = None
+    name: str | None = None
+    phone_number: str | None = None
 
 
 class UserRepository(BaseRepository[UserORM, CreateUserSchema, UpdateUserSchema]):
-    pass
+    async def get_by_username(self, username: str) -> UserORM | None:
+        stmt = select(UserORM).where(UserORM.username == username)
+        db_result = await self.db.execute(stmt)
+        return db_result.scalar_one_or_none()
