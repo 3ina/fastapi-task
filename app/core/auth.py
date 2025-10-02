296392bd2dd1e5daee843c8cbc6e40c99c23d28c
sinaroydel <3inaroydl@gmail.com>
2025-10-02 20:38:52 +0330

    add function for get user from jwt token

--- app/core/auth.py
+++ app/core/auth.py
@@ -1,9 +1,14 @@
-from app.core.config import settings
-from passlib.context import CryptContext
-from datetime import datetime, timedelta, timezone
-from jose import jwt, JWTError
-from fastapi import Depends
+from datetime import datetime, timedelta, timezone, UTC
+
+from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+
+from app.core.config import settings
+from app.core.dependencies import get_user_service
+from app.infrastructure.models import UserORM
+from app.services.userService import UserService
 
 SECRET_KEY = settings.SECRET_KEY
 ALGORITHM = "HS256"
@@ -37,7 +42,7 @@ def create_access_token(data: dict, expires_delta: int = ACCESS_TOKEN_EXPIRES_SE
     to_encode = data.copy()
     to_encode.update(
         {
-            "exp": datetime.now(timezone.utc) + timedelta(seconds=expires_delta),
+            "exp": datetime.now(UTC) + timedelta(seconds=expires_delta),
             "type": "access",
         }
     )
@@ -55,12 +60,39 @@ def create_refresh_token(
     to_encode = data.copy()
     to_encode.update(
         {
-            "exp": datetime.now(timezone.utc) + timedelta(seconds=expires_delta),
+            "exp": datetime.now(UTC) + timedelta(seconds=expires_delta),
             "type": "refresh",
         }
     )
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
-async def get_current_user(token: str = Depends(oauth2_schema)):
-    pass
+async def get_current_user(
+    token: str = Depends(oauth2_schema),
+    user_service: UserService = Depends(get_user_service),
+) -> UserORM:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        if payload.get("type") != "access":
+            raise credentials_exception
+        username: str | None = payload.get("sub")
+
+        if username is None:
+            raise credentials_exception
+
+        user = await user_service.get_by_username(username=username)
+
+        return user
+    except JWTError as e:
+        if "exp" in str(e):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Token has expired",
+                headers={"WWW-Authenticate": "Bearer"},
+            ) from e
+        raise credentials_exception from e
