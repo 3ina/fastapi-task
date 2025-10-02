84f8dfd543a507d109c4e50765b2b9b0e7416748
sinaroydel <3inaroydl@gmail.com>
2025-10-02 11:06:09 +0330

    add method for verify password

---
+++ app/core/auth.py
@@ -0,0 +1,20 @@
+from app.core.config import settings
+from passlib.context import CryptContext
+from datetime import datetime, timedelta, timezone
+
+SECERT_KEY = settings.SECRET_KEY
+ALGORITHM = "HS256"
+
+ACCESS_TOKEN_EXPIRES_SECONDS = 1200
+REFRESH_TOKEN_EXPIRES_SECONDS = 900
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def verify_password(plain_text: str, hashed_password: str) -> bool:
+    try:
+        value = pwd_context.verify(plain_text, hashed_password)
+    except ValueError:
+        return False
+    return value
