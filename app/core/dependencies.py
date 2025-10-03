b92a91faa62ebaff82b56603483f1c5041517836
sinaroydel <3inaroydl@gmail.com>
2025-10-03 19:03:47 +0330

    add dependeny for passenger repo and service

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -2,12 +2,14 @@ from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.database import get_db
-from app.infrastructure.models import UserORM
+from app.infrastructure.models import PassengerORM, UserORM
+from app.infrastructure.passengerRepository import PassengerRepository
 from app.infrastructure.userRepository import UserRepository
+from app.services.passengerService import PassengerService
 from app.services.userService import UserService
 
 
-def get_user_repo(db: AsyncSession = Depends(get_db)):
+def get_user_repo(db: AsyncSession = Depends(get_db)) -> UserRepository:
     repo = UserRepository(model=UserORM, db=db)
     return repo
 
@@ -16,3 +18,14 @@ def get_user_service(user_repo: UserRepository = Depends(get_user_repo)) -> User
     service = UserService(user_repo=user_repo)
     return service
 
+
+def get_passenger_repo(db: AsyncSession = Depends(get_db))-> PassengerRepository:
+    repo = PassengerRepository(model=PassengerORM, db=db)
+    return repo
+
+
+def get_passenger_service(
+    passenger_repo: PassengerRepository = Depends(get_passenger_repo),
+)->PassengerService:
+    service = PassengerService(passenger_repo=passenger_repo)
+    return service
