d7fc3d67c99a9d100ca74a72e21d6ebc77b8b543
sinaroydel <3inaroydl@gmail.com>
2025-10-03 18:28:45 +0330

    fix create schema for passenger

--- app/infrastructure/passengerRepository.py
+++ app/infrastructure/passengerRepository.py
@@ -1,24 +1,25 @@
 from datetime import date
-from typing import Optional
+from typing import Literal
 
 from pydantic import BaseModel
 
 from app.infrastructure.baseRepository import BaseRepository
-from app.infrastructure.models import Gender, PassengerORM
+from app.infrastructure.models import PassengerORM
 
 
 class CreatePassengerSchema(BaseModel):
+    user_id: int
     name: str
     national_id: str
     date_of_birth: date
-    gender: Gender
+    gender: Literal["men", "women"]
 
 
 class UpdatePassengerSchema(BaseModel):
     name: str | None = None
     national_id: str | None = None
     date_of_birth: date | None = None
-    gender: Gender | None = None
+    gender: Literal["men", "women"] | None = None
 
 
 class PassengerRepository(
