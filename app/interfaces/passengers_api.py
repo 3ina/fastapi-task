0614d008d21a11b7c6a75b306c8f4a2879648e18
sinaroydel <3inaroydl@gmail.com>
2025-10-03 21:11:50 +0330

    create api for get passengers

--- app/interfaces/passengers_api.py
+++ app/interfaces/passengers_api.py
@@ -5,9 +5,10 @@ from fastapi import APIRouter, Depends, status
 from pydantic import BaseModel
 
 from app.core.auth import get_current_user
-from app.core.dependencies import get_passenger_service
+from app.core.dependencies import get_passenger_service, get_passenger_selector
 from app.infrastructure.models import UserORM
 from app.services.passengerService import PassengerService
+from app.selectors.passengersSelector import PassengerSelector
 
 router = APIRouter(prefix="/passengers", tags=["Passengers"])
 
@@ -46,3 +47,21 @@ async def create_passenger(
         gender=data.gender,
         date_of_birth=data.date_of_birth,
     )
+
+
+class PassengerDataGetResponse(BaseModel):
+    id: int
+    national_id: str
+    age: int
+    gender: str
+    name: str
+
+
+@router.get(
+    "/", status_code=status.HTTP_200_OK, response_model=list[PassengerDataGetResponse]
+)
+async def get_passengers(
+    passengers_selector: PassengerSelector = Depends(get_passenger_selector),
+    current_user: UserORM = Depends(get_current_user),
+):
+    return await passengers_selector.get_passengers_for_user(user_id=current_user.id)
