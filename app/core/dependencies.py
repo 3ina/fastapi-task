3db84c85492a85ff68c9c2df295550e73c96d7ae
sinaroydel <3inaroydl@gmail.com>
2025-10-04 12:40:17 +0330

    add dependency for airport repo and selector

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -2,12 +2,14 @@ from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.database import get_db
-from app.infrastructure.models import PassengerORM, UserORM
+from app.infrastructure.models import PassengerORM, UserORM, AirportORM
+from app.infrastructure.airportRepository import AirportRepository
 from app.infrastructure.passengerRepository import PassengerRepository
 from app.infrastructure.userRepository import UserRepository
 from app.services.passengerService import PassengerService
 from app.services.userService import UserService
 from app.selectors.passengersSelector import PassengerSelector
+from app.selectors.airportSelector import AirportSelector
 
 
 def get_user_repo(db: AsyncSession = Depends(get_db)) -> UserRepository:
@@ -36,4 +38,16 @@ def get_passenger_selector(
     passenger_repo: PassengerRepository = Depends(get_passenger_repo),
 ) -> PassengerSelector:
     selector = PassengerSelector(passenger_repo=passenger_repo)
+    return selector
+
+
+def get_airport_repo(db: AsyncSession = Depends(get_db)) -> AirportRepository:
+    repo = AirportRepository(model=AirportORM, db=db)
+    return repo
+
+
+def get_airport_selector(
+    airport_repo: AirportRepository = Depends(get_airport_repo),
+) -> AirportSelector:
+    selector = AirportSelector(airport_repo=airport_repo)
     return selector
