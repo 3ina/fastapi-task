14e899900e606a93409ccbc9cfbbea842e082243
sinaroydel <3inaroydl@gmail.com>
2025-10-04 23:25:10 +0330

    fix create ticket schema

--- app/infrastructure/ticketRepository.py
+++ app/infrastructure/ticketRepository.py
@@ -1,6 +1,5 @@
-from typing import Optional
-
-from pydantic import BaseModel
+from decimal import Decimal
+from pydantic import BaseModel, Field
 
 from app.infrastructure.baseRepository import BaseRepository
 from app.infrastructure.models import TicketORM
@@ -10,12 +9,14 @@ class CreateTicketSchema(BaseModel):
     ticket_number: str
     order_id: int
     passenger_id: int
+    price: Decimal = Field(decimal_places=4, max_digits=19)
 
 
 class UpdateTicketSchema(BaseModel):
     ticket_number: str | None = None
     order_id: int | None = None
     passenger_id: int | None = None
+    price: Decimal | None = Field(decimal_places=4, max_digits=19)
 
 
 class TicketRepository(
