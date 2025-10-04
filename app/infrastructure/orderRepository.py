9b170f799f1d8905282a4a72359cc1b066426fbc
sinaroydel <3inaroydl@gmail.com>
2025-10-04 23:24:17 +0330

    update create schema order

--- app/infrastructure/orderRepository.py
+++ app/infrastructure/orderRepository.py
@@ -1,6 +1,7 @@
 from typing import Optional
-
+from datetime import datetime
 from pydantic import BaseModel
+
 
 from app.infrastructure.baseRepository import BaseRepository
 from app.infrastructure.models import OrderORM
@@ -8,14 +9,22 @@ from app.infrastructure.models import OrderORM
 
 class CreateOrderSchema(BaseModel):
     code: str
-    price: float
     user_id: int
+    destination_id: int
+    origin_id: int
+    flight_id: str
+    departure_time: datetime
+    arrival_time: datetime
 
 
 class UpdateOrderSchema(BaseModel):
     code: str | None = None
-    price: float | None = None
     user_id: int | None = None
+    destination_id: int | None = None
+    origin_id: int | None = None
+    flight_id: str | None = None
+    departure_time: datetime | None = None
+    arrival_time: datetime | None = None
 
 
 class OrderRepository(BaseRepository[OrderORM, CreateOrderSchema, UpdateOrderSchema]):
